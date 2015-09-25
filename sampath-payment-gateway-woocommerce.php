12dbf858591a85dd0ef4bcfd038a9732413daab4
Oganro <Oganro@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-09-25 08:45:11 +0000

    Oganro banner test for Sampath bank plugin
    git-svn-id: https://plugins.svn.wordpress.org/sampath-bank-ipg/trunk@1253215 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- sampath-payment-gateway-woocommerce.php
+++ sampath-payment-gateway-woocommerce.php
@@ -137,8 +137,16 @@ function woocommerce_sampath_gateway(){
     }
  
 	public function admin_options(){
+	   echo '<style type="text/css">
+		.wpimage {
+		margin:3px;
+		float:left;
+		}		
+		</style>';
     	echo '<h3>'.__('Sampath bank online payment gateway', 'ognro').'</h3>';
         echo '<p>'.__('<a target="_blank" href="http://www.oganro.com/">Oganro</a> is a fresh and dynamic web design and custom software development company with offices based in East London, Essex, Brisbane (Queensland, Australia) and in Colombo (Sri Lanka).').'</p>';
+        echo'<a href="http://www.oganro.com/support-tickets" target="_blank"><img src="/wp-content/plugins/sampath-bank-ipg/plug-inimg.jpg" alt="payment gateway" class="wpimage"/></a>';
+        
         echo '<table class="form-table">';        
         $this->generate_settings_html();
         echo '</table>'; 
